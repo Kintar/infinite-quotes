1f25d5028d3670119f0ee9637c5ea6b83e605d44
Alec Lanter <kintar1900@gmail.com>
2017-12-05 15:15:31 +0000

    Can't get the http_method on the event, so relying on API gateway to only forward GET requests.

--- quote.py
+++ quote.py
@@ -14,20 +14,19 @@ quotestable = dynamodb.Table(os.getenv('TABLE_NAME') or 'quotes')
 
 def handler(event, context):
     print(event)
-    if event['http_method']  == 'GET':
-        item = {
-            'id': str(uuid.uuid4()),
-            'date': str(datetime.datetime.now())
-        }
-        
-        try:
-            resp = quotestable.put_item(
-                Item = item
-            )
-        except ClientError as e:
-            return {'statusCode': e.response['ResponseMetadata']['HTTPStatusCode'],
-                    'body': "DynamoDB error '{}'".format(e.response['Error']['Message'])}
-        
-        return {
-            'body': "Created new quote #{} at {}".format(item['id'], item['date'])
-        }
+    item = {
+        'id': str(uuid.uuid4()),
+        'date': str(datetime.datetime.now())
+    }
+    
+    try:
+        resp = quotestable.put_item(
+            Item = item
+        )
+    except ClientError as e:
+        return {'statusCode': e.response['ResponseMetadata']['HTTPStatusCode'],
+                'body': "DynamoDB error '{}'".format(e.response['Error']['Message'])}
+    
+    return {
+        'body': "Created new quote #{} at {}".format(item['id'], item['date'])
+    }
