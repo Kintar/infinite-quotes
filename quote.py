0a83106987e6af8903dba59123c7d86d1d93909d
EC2 Default User <ec2-user@ip-172-31-60-125.ec2.internal>
2017-12-05 13:50:26 +0000

    WIP

---
+++ quote.py
@@ -0,0 +1,34 @@
+import os
+import json
+import boto3
+from botocore.exceptions import ClientError
+import uuid
+
+from datettime import datetime
+
+if os.getenv("AWS_SAM_LOCAL"):
+    dynamodb = boto3.resource('dynamodb',endpoint_url = "http://localhost:8000")
+else:
+    dynamodb = boto3.resource('dynamodb')
+
+quotestable = dynamodb.Table(os.getenv('TABLE_NAME') or 'quotes')
+
+def handler(event, context):
+    print(event)
+    if event['http_method']  == 'GET':
+        item = {
+            'id': str(uuid.uuid4()),
+            'date': str(datetime.now())
+        }
+        
+        try:
+            resp = quotestable.put_item(
+                Item = item
+            )
+        except ClientError as e:
+            return {'statusCode': e.response['ResponseMetadata']['HTTPStatusCode'],
+                    'body': "DynamoDB error '{}'".format(e.response['Error']['Message'])}
+        
+        return {
+            'body': "Created new quote #{} at {}".format(item['id'], item['date'])
+        }
