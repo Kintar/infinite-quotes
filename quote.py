23ab20a5eeff33b8873d53e223d3cfa82b5b6cae
Alec Lanter <kintar1900@gmail.com>
2017-12-05 21:01:30 +0000

    Updated logic for separate GET/PUT operations.

--- quote.py
+++ quote.py
@@ -49,26 +49,42 @@ def handler(event, context):
                 }
             
         else:
-            item = {
-                'id': str(uuid.uuid4()),
-                'date': str(datetime.datetime.now())
-            }
-            
-            try:
-                quotestable.put_item(
-                    Item = item
-                )
-            except ClientError as e:
-                print("Failed to store quote: {}".format(json.dumps(e.response['Error'])))
-                return {'statusCode': 500,
-                        'body': 'Error storing quote.  Please try again.'}
-            
             return {
-                'body': {
-                    'quoteId' : item['id'],
-                    'createdAt': item['date']
-                }
+                'statusCode': 400,
+                'body': 'Missing required path parameter "quoteId"'
             }
+    elif httpMethod == 'PUT':
+        try:
+            quote = json.loads(event['body'])
+        except:
+            return {'statusCode': 400, 'body': 'Malformed JSON input'}
+            
+        if 'lines' not in quote:
+            return {'statusCode': 400, 'body': 'Missing "lines" in request body'}
+        
+        if len(quote['lines'] == 0):
+            return {'statusCode': 400, 'body': 'No lines in quote'}
+        
+        l = 0
+        for line in quote['lines']:
+            if 'text' not in line:
+                return {'statusCode': 400, 'body': "Line {} has no text".format(l)}
+            if 'quoter' not in line:
+                return {'statusCode': 400, 'body': "Line {} has no quoter".format(l)}
+            l+=1
+        
+        quote.quoteId = str(uuid.uuid4())
+        quote.date = str(datetime.datetime.now())
+        
+        try:
+            quotestable.put_item(
+                Item = quote
+                )
+        except ClientError as e:
+            print("Failed to store quote: {}", json.dumps(e))
+            return {'statusCode': 500, 'body': 'Internal error storing quote.  Please try again.'}
+        
+        return {'statusCode': 200, 'body': quote}
     else:
         return {
             'body': "Invalid HTTP verb: {}".format(httpMethod),
