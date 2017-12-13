5248f7e503c937960d7f77c9fddfa30baf3d8e06
Alec Lanter <kintar1900@gmail.com>
2017-12-13 12:37:18 -0600

    Split website into its own repository

--- website/src/components/quotesClient.js
+++
@@ -1,7 +0,0 @@
-var Client = require('node-rest-client').Client;
-
-const quotesClient = new Client();
-
-quotesClient.registerMethod('getPage', 'https://py8n4ohrj5.execute-api.us-east-2.amazonaws.com/Prod/quotes/${group}', 'GET');
-
-export default quotesClient;
